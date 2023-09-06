4e96bcb3f08660dc8adbd27581fa5485701f7350
Farah Kia <farah.riahikia@gmail.com>
2023-09-06 23:28:07 +0100

    finished data types and constraints

--- application/models.py
+++ application/models.py
@@ -1,18 +1,9 @@
 #from application import db
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, Boolean
 
-    # order_date = db.Column(db.DateTime)
-    # status = db.Column(db.Boolean)  # True = Active cart, False = Checked out
-    # is_cart = db.Column(db.Boolean, default=False)  # True if this order is a cart
-    # description = db.Column(db.String(500), nullable=False)
-    # price = db.Column(db.Float)
-    # stock_quantity = db.Column(db.Integer)
-    # order_detail_id = db.Column(db.Integer, primary_key=True)
-    # order_id = db.Column(db.Integer, db.ForeignKey('order.order_id'), nullable=False)
-    # product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'), nullable=False)
-    # quantity = db.Column(db.Integer, nullable = False)
-    # price = db.Column(db.Float, nullable=False)
+# I used length of 255 as it's common practice (compatibility, flexibility, avoiding data truncation)
 
 class User(db.Model):
 
@@ -29,11 +20,11 @@ class Booking(db.Model):
 
     booking_id = db.Column(db.Integer, primary_key=True)
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.movie_id'))
-    screening_time = 
-    n_seats = 
+    screening_time = db.Column(db.DateTime, nullable=False)  
+    n_seats = db.Column(db.Integer, nullable=False)  
     user_email = db.Column(db.String(30), db.ForeignKey('user.user_email'))
-    ticket_type = 
-    concession = 
+    ticket_type = db.Column(db.String(20), nullable=False) 
+    concession = db.Column(db.Boolean, nullable=False)  # Assuming it's a boolean
     # order_details = db.relationship('Order_detail', backref='order')
 
 
@@ -41,9 +32,9 @@ class Movie(db.Model):
 
     movie_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), nullable=False)
-    release_date = 
-    poster_path = 
-    status = 
+    release_date = db.Column(db.DateTime, nullable=False)  
+    poster_path = db.Column(db.String(255))  
+    status = db.Column(db.String(20), nullable=False)  
     classification_id = db.Column(db.Integer, db.ForeignKey('classification.classification_id'))
     # order_details = db.relationship('Order_detail', backref='product')
 
@@ -52,38 +43,38 @@ class Payment(db.Model):
 
     payment_id = db.Column(db.Integer, primary_key=True)
     booking_id = db.Column(db.Integer, db.ForeignKey('booking.booking_id'))
-    timestamp = 
-    card_holder_name = 
-    card_number = 
-    expiry_date = 
-    security_code = 
-    amount = 
-    status = 
+    timestamp = db.Column(db.DateTime, nullable=False)  
+    card_holder_name = db.Column(db.String(50), nullable=False)  
+    card_number = db.Column(db.String(16), nullable=False)  
+    expiry_date = db.Column(db.String(5), nullable=False)  
+    security_code = db.Column(db.String(3), nullable=False) 
+    amount = db.Column(db.Float, nullable=False)  
+    status = db.Column(db.String(20), nullable=False)  
 
 
 class MenuService(db.Model):
 
     item_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-    type = 
-    price = 
-    image_path = 
+    type = db.Column(db.String(20))  
+    price = db.Column(db.Float, nullable=False)  # Assuming it's a float, or do we want to use int instead?
+    image_path = db.Column(db.String(255))  
 
 
 class DiscussionBoard(db.Model):
 
     post_id = db.Column(db.Integer, primary_key=True)
     user_email = db.Column(db.String(30), db.ForeignKey('user.user_email'))
-    content = 
-    timestamp = 
+    content = db.Column(db.String(255), nullable=False)  
+    timestamp = db.Column(db.DateTime, nullable=False)  
 
 
 class Classification(db.Model):
     
     classification_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-    icon_path = 
-    rules_and_conditions = 
+    icon_path = db.Column(db.String(255))  
+    rules_and_conditions = db.Column(db.String(255))  
 
 
 class Genre(db.Model):
@@ -104,8 +95,8 @@ class Cast(db.Model):
     cast_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
-    gender = 
-    role = 
+    gender = db.Column(db.String(10))  
+    role = db.Column(db.String(30)) 
 
 
 class MovieCast(db.Model):
@@ -118,10 +109,10 @@ class MovieCast(db.Model):
 class Screen(db.Model):
     
     screen_id = db.Column(db.Integer, primary_key=True)
-    screen_number = 
-    screen_type = 
-    capacity = 
-    seating_plan_img_path = 
+    screen_number = db.Column(db.String(5))  
+    screen_type = db.Column(db.String(20))  
+    capacity = db.Column(db.Integer, nullable=False) 
+    seating_plan_img_path = db.Column(db.String(255))  
 
 
 class MovieScreen(db.Model):
@@ -129,7 +120,7 @@ class MovieScreen(db.Model):
     movie_screen_id = db.Column(db.Integer, primary_key=True)
     screen_id = db.Column(db.Integer, db.ForeignKey('screen.screen_id'))
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.movie_id'))
-    showing_time = 
+    showing_time = db.Column(db.DateTime, nullable=False)
 
 
 
