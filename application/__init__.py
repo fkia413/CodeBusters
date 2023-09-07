a6c6111d68b746d2ecd66a2a8a30a9e4f61786bf
Farah Kia <farah.riahikia@gmail.com>
2023-09-07 12:39:02 +0100

    created working mock database and added mock data

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 # Create the Flask application
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # Configure the Flask app with a SQLite database URI
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:\\\Users\xmika\Codebusters\Codebusters' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mock_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking modifications (optional)
 app.config['SECRET_KEY'] = 'your_secret_key_here'  # Replace with a secret key
@@ -16,4 +19,3 @@ db = SQLAlchemy(app)
 
 # Import your routes and models
 from application import routes, models
-
