fb992ca541c3d84289bf1e6f7bd820d24dd34da4
CuriousCr0w <126788507+c-wright-98@users.noreply.github.com>
2023-09-12 09:13:37 +0100

    CW: Long commit message
    Done: Updated the services page to connect to the database
    and retrieve the menu items from there and loop them into product cards,
    worked on the styling of them a bit as well.
    
    Fixed the input text to be black to be visible and edited the
    styling of the login and registration (This one still needs work) to be fit the rest of the website.
    The backgrounds are still not showing on these pages.
    
    TO-Do: Still working on the booking system as the screenigns are still
    not showing but roberto has made the movie details page that will make
    things easier as he has got the screenings to show with filter.
    Need to implement using the url variables of movie id and screening id
    to automatically fill out booking form.
    Make tickets not go to negative.

--- application/static/js/booking.js
+++ application/static/js/booking.js
@@ -1,24 +1,71 @@
-// Get references to the movie and screening time select fields
-const movieSelect = document.getElementById('{{ form.movie_id.id }}');
-const screeningTimeSelect = document.getElementById(
-	'{{ form.screening_time.id }}'
-);
-
 // Define an event listener to update the screening time choices when the movie is selected
-movieSelect.addEventListener('change', () => {
-	const selectedMovieId = movieSelect.value;
-	// Make an AJAX request to fetch the screening times for the selected movie
-	fetch(`/get_screening_times?movie_id=${selectedMovieId}`)
-		.then((response) => response.json())
-		.then((data) => {
-			// Clear the current options
-			screeningTimeSelect.innerHTML = '';
-			// Populate the screening time options based on the retrieved data
-			data.forEach((option) => {
-				const newOption = document.createElement('option');
-				newOption.value = option[0];
-				newOption.textContent = option[1];
-				screeningTimeSelect.appendChild(newOption);
-			});
-		});
+$(document).ready(function () {
+    const movieSelect = $('#{{ form.movie_id.id }}');
+    const screeningTimeSelect = $('#{{ form.screening_time.id }}');
+    const adultTicketsInput = $('#{{ form.adult_tickets.id }}');
+    const childTicketsInput = $('#{{ form.child_tickets.id }}');
+    const totalPriceInput = $('#{{ form.total_price.id }}');
+
+    // Function to update the total price
+    function updateTotalPrice() {
+        // Fetch ticket prices from the server using an AJAX request
+        $.ajax({
+            url: '/get_ticket_prices',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Replace placeholders with actual ticket prices
+                const adultTicketPrice = parseFloat(data.adult_price);
+                const childTicketPrice = parseFloat(data.child_price);
+
+                const adultTickets = parseInt(adultTicketsInput.val()) || 0;
+                const childTickets = parseInt(childTicketsInput.val()) || 0;
+
+                const total = adultTicketPrice * adultTickets + childTicketPrice * childTickets;
+                totalPriceInput.val(total.toFixed(2)); // Update the total price input field
+            },
+            error: function (error) {
+                console.error('Error fetching ticket prices:', error);
+            },
+        });
+    }
+
+    // Initial update of total price
+    updateTotalPrice();
+
+    // Event listener for movie selection
+    movieSelect.on('change', () => {
+        const selectedMovieId = movieSelect.val();
+
+        // Make an AJAX request to fetch the screening times for the selected movie
+        $.ajax({
+            url: `/get_screening_times?movie_id=${selectedMovieId}`,
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Clear the current options
+                screeningTimeSelect.empty();
+
+                // Populate the screening time options based on the retrieved data
+                $.each(data, function (index, option) {
+                    screeningTimeSelect.append(
+                        $('<option>', {
+                            value: option[0],
+                            text: option[1],
+                        })
+                    );
+                });
+            },
+            error: function (error) {
+                console.error('Error fetching screening times:', error);
+            },
+        });
+    });
+
+    // Event listener for adult and child ticket inputs
+    adultTicketsInput.on('input', updateTotalPrice);
+    childTicketsInput.on('input', updateTotalPrice);
 });
+
+
+
